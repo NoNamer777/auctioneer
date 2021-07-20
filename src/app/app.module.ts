5febe7f4082ca5a845d541b18c3436f7804b6697
Oscar Wellner <oscar.wellner@gmail.com>
2021-07-20 18:10:06 +0200

    Add mainpage layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { FaIconsModule } from './modules/fa-icons.module';
+import { FaIconsModule } from '@modules/fa-icons.module';
+
+import { AppComponent } from '@app/app.component';
+import { HeaderComponent } from '@components/mainpage/header/header.component';
+import { HomeComponent } from '@components/mainpage/home/home.component';
+import { NavBarComponent } from '@components/mainpage/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/mainpage/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auc-header',
+  templateUrl: './header.component.html',
+  styleUrls: [ './header.component.scss' ],
+})
+export class HeaderComponent {}
